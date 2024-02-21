d9eb98fcd85b7ce013706145cf80abcafd4cb8b3
dzackiero <dzakynashshar@gmail.com>
2024-02-21 17:29:01 +0700

    Add TopBarIconButton Component

---
+++ app/src/main/java/com/coder/siakad/presentation/component/TopBarIconButton.kt
@@ -0,0 +1,29 @@
+package com.coder.siakad.presentation.component
+
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+
+@Composable
+fun TopBarIconButton(
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    onClick: () -> Unit,
+    shape: Shape = IconButtonDefaults.filledShape,
+    content: @Composable () -> Unit,
+) {
+    FilledIconButton(
+        onClick = onClick,
+        colors = IconButtonDefaults.filledIconButtonColors(
+            containerColor = MaterialTheme.colorScheme.background,
+            contentColor = MaterialTheme.colorScheme.primary
+        ),
+        modifier = modifier,
+        shape = shape,
+        enabled = enabled,
+        content = content
+    )
+}
