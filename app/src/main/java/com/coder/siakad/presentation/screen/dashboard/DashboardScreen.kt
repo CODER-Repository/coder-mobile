34d4c5d6d695efe7ee67624d2babf91aa1b73f3c
dzackiero <dzakynashshar@gmail.com>
2024-02-28 11:02:01 +0700

    CSK 47 - feat: add DashboardScreen TopBar

--- app/src/main/java/com/coder/siakad/presentation/screen/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/coder/siakad/presentation/screen/dashboard/DashboardScreen.kt
@@ -2,20 +2,26 @@ package com.coder.siakad.presentation.screen.dashboard
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.MoreHoriz
 import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
@@ -24,68 +30,108 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.coder.siakad.presentation.component.TopBarIconButton
-import com.coder.siakad.ui.theme.PrimaryBlue500
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DashboardScreen() {
     Scaffold(
         topBar = {
-            CenterAlignedTopAppBar(
-                modifier = Modifier
-                    .background(PrimaryBlue500)
-                    .padding(horizontal = 8.dp),
-                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-                    containerColor = PrimaryBlue500,
-                    titleContentColor = Color.White,
-                ),
-                title = {
-                    Text(
-                        text = "Beranda",
-                        fontWeight = FontWeight.SemiBold,
-                        fontSize = 24.sp,
-                    )
-                },
-                navigationIcon = {
-                    TopBarIconButton(onClick = { /*TODO*/ }) {
-                        Icon(
-                            imageVector = Icons.Outlined.Notifications,
-                            contentDescription = "notification",
-                        )
-                    }
-                },
-                actions = {
-                    TopBarIconButton(onClick = { /*TODO*/ }) {
-                        Icon(
-                            imageVector = Icons.Default.MoreVert,
-                            contentDescription = "notification",
-                        )
-                    }
-                }
-            )
-        }
-    ) { paddingValues ->
-        Column {
-
             Column(
                 modifier = Modifier
-                    .padding(paddingValues)
-                    .background(Color(0xFF245399))
-                    .padding(16.dp)
-                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.primary)
+                    .padding(horizontal = 18.dp)
+                    .padding(bottom = 24.dp),
+                verticalArrangement = Arrangement.spacedBy(24.dp)
             ) {
-                Box(
-                    modifier = Modifier
-                        .size(64.dp)
-                        .border(4.dp, Color.White, CircleShape)
-                        .clip(CircleShape)
-                        .background(Color.LightGray)
+                CenterAlignedTopAppBar(
+                    colors = TopAppBarDefaults
+                        .centerAlignedTopAppBarColors(
+                            containerColor = MaterialTheme.colorScheme.primary,
+                            titleContentColor = Color.White,
+                        ),
+                    title = {
+                        Text(
+                            text = "Beranda",
+                            fontWeight = FontWeight.SemiBold,
+                            fontSize = 24.sp,
+                        )
+                    },
+                    navigationIcon = {
+                        TopBarIconButton(onClick = { /*TODO*/ }) {
+                            Icon(
+                                imageVector = Icons.Outlined.Notifications,
+                                contentDescription = "notification",
+                            )
+                        }
+                    },
+                    actions = {
+                        TopBarIconButton(onClick = { /*TODO*/ }) {
+                            Icon(
+                                imageVector = Icons.Default.MoreHoriz,
+                                contentDescription = "notification",
+                            )
+                        }
+                    }
                 )
-
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    // TODO: Change this box to AsyncImage
+                    Row(
+                        modifier = Modifier.height(IntrinsicSize.Min),
+                        horizontalArrangement = Arrangement.spacedBy(16.dp)
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .size(72.dp)
+                                .border(4.dp, Color.White, CircleShape)
+                                .clip(CircleShape)
+                                .background(Color.LightGray)
+                        )
+                        Column(
+                            modifier = Modifier
+                                .fillMaxHeight(),
+                            verticalArrangement = Arrangement.SpaceAround
+                        ) {
+                            Column {
+                                Text(
+                                    text = "Husni Mubarok",
+                                    style = MaterialTheme.typography.labelLarge,
+                                    fontWeight = FontWeight.SemiBold,
+                                    color = MaterialTheme.colorScheme.onPrimary,
+                                    maxLines = 1,
+                                    overflow = TextOverflow.Ellipsis
+                                )
+                                Text(
+                                    text = "Informatika",
+                                    style = MaterialTheme.typography.labelSmall,
+                                    color = MaterialTheme.colorScheme.onPrimary,
+                                    maxLines = 1,
+                                    overflow = TextOverflow.Ellipsis
+                                )
+                            }
+                            Card(shape = MaterialTheme.shapes.small) {
+                                Text(
+                                    text = "Reguler",
+                                    fontWeight = FontWeight.Medium,
+                                    style = MaterialTheme.typography.labelSmall,
+                                    color = MaterialTheme.colorScheme.primary,
+                                    modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
+                                )
+                            }
+                        }
+                    }
+                }
             }
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .padding(16.dp)
+        ) {
             Column {
                 CardDefaults.cardColors()
                 Card {
